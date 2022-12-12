33fea7c8aa6832348dc76f8ec98693e7451acb8a
Chet Jogia <chetjogia@gmail.com>
2022-12-12 10:57:49 +0000

    added graph to modal

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -1,21 +1,38 @@
 import React from "react";
 import { useState, useEffect } from "react";
 import "./index.css";
+import {
+  LineChart,
+  Line,
+  CartesianGrid,
+  XAxis,
+  YAxis,
+  Tooltip,
+  Legend,
+} from "recharts";
 
 function Modal() {
   const [modal, setModal] = useState(false);
-  const [data, setData] = useState(null);
+  const [childData, setChildData] = useState(null);
   const [isloading, setIsLoading] = useState(true);
+  const [graphData, setGraphData] = useState([])
+  
 
   useEffect(() => {
     async function getData() {
-      const response = await fetch("http://localhost:3000/api/babymonitor/feeding");
+      const response = await fetch(
+        "http://localhost:3000/api/babymonitor/feeding"
+      );
       const data = await response.json();
-      setData(data.payload);
+      setChildData(data.payload);
       setIsLoading(false);
+      let graph = await data.payload.map(element=>{return {"time": element.feeding_time, y: Number(element.quantity)}})
+      setGraphData(graph)
+      console.log(graph)
+     
     }
     getData();
-  },[]);
+  }, []);
 
   function toggleModal() {
     setModal(!modal);
@@ -36,12 +53,26 @@ function Modal() {
           </div>
         ) : (
           <>
+          
             <div className="modal">
               <div onClick={toggleModal} className="overlay"></div>
               <div className="modal-content">
-                {console.log("DATA", data)}
                 <button onClick={toggleModal}>Close</button>
-                <h1 className="title">{data.payload[0].type}</h1>
+                <h1 className="title">Feeding Data</h1>
+                
+                <LineChart
+                  width={1000}
+                  height={250}
+                  data={graphData}
+                  margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
+                >
+                  <CartesianGrid strokeDasharray="3 3" />
+                  <XAxis dataKey="time" />
+                  <YAxis />
+                  <Tooltip />
+                  <Legend />
+                  <Line type="monotone" dataKey="y" stroke="#8884d8" />
+                </LineChart>
               </div>
             </div>
           </>
